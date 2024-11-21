8afe1354ac2647857385d662d1c980b21b7e02c3
VitorBurratto <vitorabd@gmail.com>
2024-11-21 11:22:53 -0300

    vitor/fix/correção-models
    - Fix: correção de erro nos models de PostFeed, o qual, não estáva salvando nas contas de cada perfil

--- plataformav/models.py
+++ plataformav/models.py
@@ -33,21 +33,25 @@ class Post(models.Model):
 class PostFeed(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    
+
     def __str__(self):
-        return self.post
+        return f"Post Feed para {self.account.name}"
+    
 
 class Comment(models.Model):
-    content = models.TextField(verbose_name='Conteúdo do Comentário')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(verbose_name='Conteúdo do Comentário')
     createdAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.content
+        return f"Comentário de {self.account.name} no Post {self.post.code}"
 
 
+# Sinal para criar um PostFeed automaticamente quando um novo Post é criado
 @receiver(post_save, sender=Post)
-def create_post_feed(sender, instance, created, **kwargs):  
+def createPostFeed(sender, instance, created, **kwargs):
     if created:
-        PostFeed.objects.create(account=instance.account, post=instance)
+        # Cria um PostFeed somente para a conta do autor do post
+        if instance.account:
+            PostFeed.objects.create(account=instance.account, post=instance)
