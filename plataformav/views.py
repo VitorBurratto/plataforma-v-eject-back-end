0dc8895f2e4cbeefde5191c0058b0d7ae5ea2f73
VitorBurratto <vitorabd@gmail.com>
2024-11-22 11:00:51 -0300

    vitor/feat/protecao-jtw
    - Feat: Criação da proteção por token JWT

--- plataformav/views.py
+++ plataformav/views.py
@@ -1,5 +1,4 @@
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated 
 from rest_framework import viewsets, generics
 from plataformav.models import Account, Post, PostFeed, Comment
 from plataformav.serializers import AccountSerializer, PostSerializer, PostFeedSerializer, CommentSerializer
@@ -7,17 +6,21 @@ from plataformav.serializers import AccountSerializer, PostSerializer, PostFeedS
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+    permission_classes = [IsAuthenticated,] 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostSerializer 
+    permission_classes = [IsAuthenticated,]
 
 class PostFeedViewSet(viewsets.ModelViewSet):
     queryset = PostFeed.objects.all()
     serializer_class = PostFeedSerializer
+    permission_classes = [IsAuthenticated,]
 
 class ListPostFeedView(generics.ListAPIView):
     serializer_class = PostFeedSerializer
+    permission_classes = [IsAuthenticated,]
 
     def get_queryset(self):
         return PostFeed.objects.filter(account_id=self.kwargs['pk'])
@@ -25,3 +28,4 @@ class ListPostFeedView(generics.ListAPIView):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated,]
--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from plataformav.views import (AccountViewSet,PostViewSet,PostFeedViewSet,ListPostFeedView, CommentViewSet)
 from rest_framework import routers
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 router.register('accounts', AccountViewSet, basename='Accounts')
@@ -13,4 +16,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('accounts/<int:pk>/postfeeds/', ListPostFeedView.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
